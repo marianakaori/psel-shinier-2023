a719e791a28ecf1382d7a168fbae4ec4aca6b8c7
Mariana Kaori <mariana.kaori@usp.br>
2023-03-28 18:19:07 -0300

    estudando o banco de dados

---
+++ scripts/script_teste1.sql
@@ -0,0 +1,5 @@
+SELECT '' AS nome_da_clinica, e.NOME AS nome_do_paciente, c.documento AS Descricao_do_lancamento, c.valor AS Valor_a_pagar, b.VALOR AS valor_pago, c.EMISSAO AS data_criacao_lancamento, c.VENCTO AS data_vencimento, b.TRANSMISSAO AS confirmacao_pagamento, b.BAIXA AS data_recebimento, c.SITUACAO 
+FROM EMD101 e JOIN CRD111 c ON e.CGC_CPF = c.CGC_CPF 
+LEFT JOIN BXD111 b ON b.DOCUMENTO = c.DOCUMENTO 
+WHERE e.CLIENTE = 'S'
+ORDER BY e.NOME 
